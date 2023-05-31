b8b5d83c62ebd57199be0ea35c57c5ef73e76cc3
ibifaa <ibifaaibisakiaruoture@gmail.com>
2023-05-31 13:43:32 +0100

    Added functionality to button Added and Remove Classlist using if command

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const dot = document.querySelector("myButtons")
+const button = document.querySelector(".button")
+const links = document.querySelector(".links")
+// if(window.innerWidth>=500){
+//     dot.classList.remove("dot-section");
+// }
 
-if(window.innerWidth>=500){
-    dot.classList.remove("dot-section");
-}
+button.addEventListener("click", ()=>{
+    // console.log(links.classList);
+//    const myLinks = links.classList.contains("hide-links");
+   if(links.classList.contains("hide-links")){
+    links.classList.remove("hide-links");
+   }
+   else{
+    links.classList.add("hide-links");
+   }
+})
